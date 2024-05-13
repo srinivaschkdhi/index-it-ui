e1afac302dca7777f86ec33d7f6d6e8ad5c9cec1
Srinivas Chintakindhi <srinivas.c@ayu.health>
2024-05-13 16:38:52 +0530

    fix pdf view layout issue

--- src/components/PDFViewer.js
+++ src/components/PDFViewer.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 function PDFViewer({ fileUrl }) {
     return (
-        <iframe src={fileUrl} width="100%" height="500px" />
+        <iframe 
+            src={fileUrl} 
+            style={{ width: '100%', height: '100%', border: 'none' }} // Added border: 'none' for cleaner look
+        />
     );
 }
 
